f11a1cfe37e7173df96e42e3bbf6081b91c58f44
clementjumel <clement.jumel@gmail.com>
2024-11-15 16:22:39 +0100

    Upgrade package version to 0.1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="linkup-sdk",
-    version="0.1.3",
+    version="0.1.4",
     author="LINKUP TECHNOLOGIES",
     author_email="contact@linkup.so",
     description="A Python Client SDK for the Linkup API",
