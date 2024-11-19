8e933a556be5492335f538a1c1de490a074f34c5
clementjumel <clement.jumel@gmail.com>
2024-11-19 18:31:43 +0100

    Small fixes to pass the tests

--- linkup/__init__.py
+++ linkup/__init__.py
@@ -1,12 +1,25 @@
-from .client import LinkupClient
-from .errors import LinkupAuthenticationError, LinkupInvalidRequestError, LinkupUnknownError
-from .types import LinkupSearchResult, LinkupSearchResults, LinkupSource, LinkupSourcedAnswer
+from .client import (
+    LinkupClient,
+)
+from .errors import (
+    LinkupAuthenticationError,
+    LinkupInvalidRequestError,
+    LinkupUnknownError,
+)
+from .types import (
+    LinkupContent,
+    LinkupSearchResult,
+    LinkupSearchResults,
+    LinkupSource,
+    LinkupSourcedAnswer,
+)
 
 __all__ = [
     "LinkupClient",
     "LinkupAuthenticationError",
     "LinkupInvalidRequestError",
     "LinkupUnknownError",
+    "LinkupContent",
     "LinkupSearchResult",
     "LinkupSearchResults",
     "LinkupSource",
